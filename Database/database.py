5376a46b5e088a1e18fce76b6321511d5275cf80
StudentPK123 <pk.student123@protonmail.com>
2022-01-13 18:48:03 +0100

    list of cities and train connections

---
+++ Database/database.py
@@ -0,0 +1,25 @@
+#list of cities with railway stations
+CITY_LIST = [
+    "Kraków",
+    "Katowice",
+    "Wrocław",
+    "Białogard",
+    "Ciechanów",
+    "Białystok",
+    "Giżycko",
+    "Gniezno",
+    "Poznań",
+    "Kalisz",
+    "Kielce",
+    "Kołobrzeg"
+]
+
+#list of train connections between the various cities 
+RAILWAY_CONNECTIONS = [
+    ("Kraków","Wrocław"),
+    ("Kraków","Gniezno"),
+    ("Wrocław","Kalisz"),
+    ("Kołobrzeg","Poznań"),
+    ("Gniezno","Kołobrzeg"),
+    ("Kołobrzeg","Kraków")
+]
