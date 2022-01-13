a9c0339a81ac79d7f4f8bdf80f7ca1068a910237
StudentPK123 <pk.student123@protonmail.com>
2022-01-13 19:27:02 +0100

    button and radiobutton on MainWindow

--- UI/windows.py
+++ UI/windows.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 from tkinter import ttk
 
+from Graphs.graphs import *
+
 class MainWindow():
     def __init__(self):
         self.root = tk.Tk()
@@ -16,5 +18,35 @@ class MainWindow():
         tab_control.add(tab_setting, text='Ustawienia połączeń')
         tab_control.add(tab_search_connection, text='Wyszukiwanie połączeń')
         tab_control.pack(expand=1, fill="both")
+
+        label1 = tk.Label(tab_setting, text="Miejsce początkowe")
+        label1.pack(padx=5, pady=5)
+
+        self.start_combo = ttk.Combobox(tab_setting, values=CITY_LIST)
+        self.start_combo.pack(padx=25, pady=5)
+
+        label2 = tk.Label(tab_setting, text="Miejsce docelowe")
+        label2.pack(padx=5, pady=5)
+
+        self.destination_combo = ttk.Combobox(tab_setting, values=CITY_LIST)
+        self.destination_combo.pack(padx=25, pady=5)
+
+        #connect/disconnect button
+        button1 = tk.Button(tab_setting, text="Połącz", command= lambda: self.manage_connection_button_handler(True))
+        button1.pack(padx=5, pady=5)
+        button2 = tk.Button(tab_setting, text="Rołącz", command= lambda: self.manage_connection_button_handler(False))
+        button2.pack(padx=5, pady=5)
+
+        #selecting a neighborhood type
+        var = tk.IntVar()
+        r1 = tk.Radiobutton(tab_setting, text="Macierz sąsiedztwa", variable=var, value=1,
+                            command=lambda: self.update_graph(self.graph_neighborhood_matrix))
+        r1.pack(anchor=tk.W)
+        r1.select()
+
+        r2 = tk.Radiobutton(tab_setting, text="Listy sąsiedztwa", variable=var, value=2,
+                            command=lambda: self.update_graph(self.graph_adjacency_lists))
+        r2.pack(anchor=tk.W)
+
 
         self.root.mainloop()
